8153af69ce675e4bb161a1f92144f8ac70669b1e
bydavidmarx <166698106+bydavidmarx@users.noreply.github.com>
2026-03-17 12:28:31 +0000

    Update main.py

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ app = FastAPI(title="PPS API", version="1.0.0")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
